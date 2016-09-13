babf5689835da120f0e78e2bf0a8d28e9071c75b
ilyablinov <ilya@Mac-Pro-ilyablinov.local>
2016-09-13 09:28:59 -0700

    PROJECT: CREATED : New CLASS: IBServerManager

---
+++ VkAudioApp/IBServerManager.h
@@ -0,0 +1,13 @@
+//
+//  IBServerManager.h
+//  VkAudioApp
+//
+//  Created by ilyablinov on 13.09.16.
+//  Copyright (c) 2016 IB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IBServerManager : NSObject
+
+@end
---
+++ VkAudioApp/IBServerManager.m
@@ -0,0 +1,13 @@
+//
+//  IBServerManager.m
+//  VkAudioApp
+//
+//  Created by ilyablinov on 13.09.16.
+//  Copyright (c) 2016 IB. All rights reserved.
+//
+
+#import "IBServerManager.h"
+
+@implementation IBServerManager
+
+@end
